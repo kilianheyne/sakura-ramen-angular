29702ff0ff86fa476f53299a18cfca1ac9b84cbe
Kilian Heyne <kilianheyne@icloud.com>
2025-06-11 17:16:32 +0200

    added a footer and a-links with IDs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainContentComponent, FooterComponent, ImprintComponent],
+  imports: [RouterOutlet, MainContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LandingPageContent } from './landingPage/landingPage.component';
-import { OurMenuComponent } from './our-menu/our-menu.component';
 import { HowToOrderComponent } from './how-to-order/how-to-order.component';
+import { OurMenuComponent } from './our-menu/our-menu.component';
+
 
 @Component({
   selector: 'app-main-content',
-  imports: [LandingPageContent, OurMenuComponent, HowToOrderComponent],
+  imports: [LandingPageContent, HowToOrderComponent, OurMenuComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
